1b46087291b56fb35a2af47b676c55736202f7e4
yourfriend <81387799+yourfriendoss@users.noreply.github.com>
2022-03-19 12:32:27 +0200

    im smart xdddddddd

--- deps.ts
+++ deps.ts
@@ -2,5 +2,5 @@ export * from "https://deno.land/x/sqlite/mod.ts";
 export { Collection } from "https://deno.land/x/harmony/src/utils/collection.ts";
 export { Pool } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
 export { PoolClient } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
-export { QueryObjectResult } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
+export { QueryObjectResult } from "https://deno.land/x/postgres@v0.15.0/query/query.ts";
 export { ld as _ } from "https://deno.land/x/deno_lodash/mod.ts";
