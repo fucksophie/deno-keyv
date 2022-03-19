9a2555e2f67790bf8c1bfa15d11c31287ea88544
yourfriend <81387799+yourfriendoss@users.noreply.github.com>
2022-03-19 12:50:03 +0200

    downgrade sqlite version 2 work

--- deps.ts
+++ deps.ts
@@ -1,6 +1,6 @@
-export * from "https://deno.land/x/sqlite/mod.ts";
-export { Collection } from "https://deno.land/x/harmony/src/utils/collection.ts";
+export * from "https://deno.land/x/sqlite@v2.5.0/mod.ts";
+export { Collection } from "https://deno.land/x/harmony@v2.6.0/src/utils/collection.ts";
 export { Pool } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
 export { PoolClient } from "https://deno.land/x/postgres@v0.15.0/mod.ts";
 export { QueryObjectResult } from "https://deno.land/x/postgres@v0.15.0/query/query.ts";
-export { ld as _ } from "https://deno.land/x/deno_lodash/mod.ts";
+export { ld as _ } from "https://deno.land/x/deno_lodash@v0.1.0/mod.ts";
